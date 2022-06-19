f3f7cbc452c93a7acfc835b1ee12c59c9a87d7c5
noyize <iamnoyaljose@gmail.com>
2022-06-19 22:08:10 +0530

    Create SpaceFact.kt for domain representation

---
+++ app/src/main/java/com/noyize/deepsky/domain/model/SpaceFact.kt
@@ -0,0 +1,9 @@
+package com.noyize.deepsky.domain.model
+
+data class SpaceFact(
+    val date: String,
+    val explanation: String,
+    val hdImageUrl: String,
+    val title: String,
+    val imageUrl: String
+)
