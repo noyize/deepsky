b766aff61609d7ef6f5ae3b0cfd82f9999eabe76
noyize <iamnoyaljose@gmail.com>
2022-06-19 23:32:24 +0530

    Setup viewBinding for `FactsFragment`

--- app/src/main/java/com/noyize/deepsky/presentation/facts/FactsFragment.kt
+++ app/src/main/java/com/noyize/deepsky/presentation/facts/FactsFragment.kt
@@ -6,8 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.noyize.deepsky.R
+import com.noyize.deepsky.databinding.FragmentFactsBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class FactsFragment : Fragment(R.layout.fragment_facts) {
 
+    private lateinit var binding : FragmentFactsBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentFactsBinding.bind(view)
+    }
 }
