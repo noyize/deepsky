a01bb3edfd2bbd940371c2c504cd522afd71fc44
noyize <iamnoyaljose@gmail.com>
2022-06-19 23:28:59 +0530

    Setup viewBinding for `MainActivity`

--- app/src/main/java/com/noyize/deepsky/presentation/main/MainActivity.kt
+++ app/src/main/java/com/noyize/deepsky/presentation/main/MainActivity.kt
@@ -3,10 +3,17 @@ package com.noyize.deepsky.presentation.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.noyize.deepsky.R
+import com.noyize.deepsky.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
