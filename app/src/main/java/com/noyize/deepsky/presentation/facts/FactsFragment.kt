5200c010595b711b4533bc0fe60f30acffcff85a
noyize <iamnoyaljose@gmail.com>
2022-06-19 23:37:08 +0530

    Setup recyclerview for `FactsFragment`

--- app/src/main/java/com/noyize/deepsky/presentation/facts/FactsFragment.kt
+++ app/src/main/java/com/noyize/deepsky/presentation/facts/FactsFragment.kt
@@ -5,17 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.noyize.deepsky.R
 import com.noyize.deepsky.databinding.FragmentFactsBinding
+import com.noyize.deepsky.presentation.main.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class FactsFragment : Fragment(R.layout.fragment_facts) {
+class FactsFragment : Fragment(R.layout.fragment_facts),SpaceFactAdapter.ClickListener {
 
     private lateinit var binding : FragmentFactsBinding
+    private val mainViewModel by activityViewModels<MainViewModel>()
+    private val spaceFactAdapter by lazy { SpaceFactAdapter(this) }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentFactsBinding.bind(view)
+        binding.recyclerView.adapter = spaceFactAdapter
+        spaceFactAdapter.submitList(mainViewModel.spaceFacts)
+    }
+
+    override fun onClick(position: Int) {
+        TODO("Not yet implemented")
     }
 }
