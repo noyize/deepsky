c128dd697dbcf686f1aa8187d1b5767e1ba96b24
noyize <iamnoyaljose@gmail.com>
2022-06-19 22:59:28 +0530

    Add `FactsFragment`

---
+++ app/src/main/java/com/noyize/deepsky/presentation/facts/FactsFragment.kt
@@ -0,0 +1,13 @@
+package com.noyize.deepsky.presentation.facts
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.noyize.deepsky.R
+
+
+class FactsFragment : Fragment(R.layout.fragment_facts) {
+
+}
