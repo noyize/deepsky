2e87e5543ed3e6a3b434d850ee28aee8b8b3be40
noyize <iamnoyaljose@gmail.com>
2022-06-19 23:03:49 +0530

    Add `DetailFragment`

---
+++ app/src/main/java/com/noyize/deepsky/presentation/detail/DetailFragment.kt
@@ -0,0 +1,13 @@
+package com.noyize.deepsky.presentation.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.noyize.deepsky.R
+
+
+class DetailFragment : Fragment(R.layout.fragment_detail) {
+
+}
