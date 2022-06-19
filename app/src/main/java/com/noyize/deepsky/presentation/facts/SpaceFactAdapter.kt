804610fbcccc67379ac7bb7029c07103322e608f
noyize <iamnoyaljose@gmail.com>
2022-06-19 23:27:21 +0530

    Create `SpaceFactAdapter`

---
+++ app/src/main/java/com/noyize/deepsky/presentation/facts/SpaceFactAdapter.kt
@@ -0,0 +1,48 @@
+package com.noyize.deepsky.presentation.facts
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.noyize.deepsky.databinding.ItemSpaceFactBinding
+import com.noyize.deepsky.domain.model.SpaceFact
+
+class SpaceFactAdapter(private val listener: ClickListener) :
+    ListAdapter<SpaceFact, RecyclerView.ViewHolder>(DiffCallback) {
+
+    interface ClickListener {
+        fun onClick(position: Int)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val binding =
+            ItemSpaceFactBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return SpaceFactViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as SpaceFactViewHolder).bind(getItem(position), position)
+    }
+
+    inner class SpaceFactViewHolder(private val binding: ItemSpaceFactBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(spaceFact: SpaceFact, position: Int) {
+            with(binding) {
+                thumbnail.load(spaceFact.imageUrl)
+                root.setOnClickListener { listener.onClick(position) }
+            }
+        }
+    }
+}
+
+object DiffCallback : DiffUtil.ItemCallback<SpaceFact>() {
+    override fun areItemsTheSame(oldItem: SpaceFact, newItem: SpaceFact): Boolean {
+        return oldItem.imageUrl == newItem.imageUrl
+    }
+
+    override fun areContentsTheSame(oldItem: SpaceFact, newItem: SpaceFact): Boolean {
+        return oldItem == newItem
+    }
+}
