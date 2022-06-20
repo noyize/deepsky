351625a4b5227b4e35f2f24f1acf2d28094b2516
noyize <iamnoyaljose@gmail.com>
2022-06-20 12:19:57 +0530

    Enable bottom sheet dismiss on outside touch

--- app/src/main/java/com/noyize/deepsky/presentation/detail/sheet/DetailBottomSheet.kt
+++ app/src/main/java/com/noyize/deepsky/presentation/detail/sheet/DetailBottomSheet.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -33,7 +34,6 @@ class DetailBottomSheet : BottomSheetDialogFragment() {
         val bottomSheetDialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
         bottomSheetDialog.setOnShowListener { dialog ->
             val bottomDialog = dialog as BottomSheetDialog
-            bottomDialog.setCanceledOnTouchOutside(false)
             val bottomSheet =
                 bottomDialog.findViewById<FrameLayout>(com.google.android.material.R.id.design_bottom_sheet) as FrameLayout
             BottomSheetBehavior.from(bottomSheet).state =
@@ -47,6 +47,7 @@ class DetailBottomSheet : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         observeUI()
+        binding.closeButton.setOnClickListener { findNavController().navigateUp() }
     }
 
     private fun observeUI() {
