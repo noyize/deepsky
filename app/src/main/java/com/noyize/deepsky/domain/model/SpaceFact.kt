f79bae7cf0ef17bb5e30ad8d7fff7fceae7b2b2c
noyize <iamnoyaljose@gmail.com>
2022-06-20 12:04:20 +0530

    Make `SpaceFact` parcelable

--- app/src/main/java/com/noyize/deepsky/domain/model/SpaceFact.kt
+++ app/src/main/java/com/noyize/deepsky/domain/model/SpaceFact.kt
@@ -1,9 +1,13 @@
 package com.noyize.deepsky.domain.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class SpaceFact(
     val date: String,
     val explanation: String,
     val hdImageUrl: String,
     val title: String,
     val imageUrl: String
-)
+) : Parcelable
