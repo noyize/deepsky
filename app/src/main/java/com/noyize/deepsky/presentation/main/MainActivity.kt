34c55c813dc200624446098db348cb3c5f0b080b
noyize <iamnoyaljose@gmail.com>
2022-06-20 20:47:17 +0530

    Fix splashscreen not showing

--- app/src/main/java/com/noyize/deepsky/presentation/main/MainActivity.kt
+++ app/src/main/java/com/noyize/deepsky/presentation/main/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         WindowCompat.setDecorFitsSystemWindows(window,false)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
