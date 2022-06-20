41576091b2477f9f90bc67b918812b7cfc857b5b
noyize <iamnoyaljose@gmail.com>
2022-06-20 10:59:34 +0530

    Setup appbar with navigation

--- app/src/main/java/com/noyize/deepsky/presentation/main/MainActivity.kt
+++ app/src/main/java/com/noyize/deepsky/presentation/main/MainActivity.kt
@@ -2,6 +2,9 @@ package com.noyize.deepsky.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.noyize.deepsky.R
 import com.noyize.deepsky.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,5 +18,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        setUpNavigation()
+    }
+
+    private fun setUpNavigation(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.toolbar.setupWithNavController(navController)
     }
 }
