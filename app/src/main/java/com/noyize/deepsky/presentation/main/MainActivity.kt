cc8f7ec354f00e4c2575b78ff0a2fc4b065c0694
noyize <iamnoyaljose@gmail.com>
2022-06-21 14:10:45 +0530

    Code cleanup

--- app/src/main/java/com/noyize/deepsky/presentation/main/MainActivity.kt
+++ app/src/main/java/com/noyize/deepsky/presentation/main/MainActivity.kt
@@ -1,44 +1,22 @@
 package com.noyize.deepsky.presentation.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupWithNavController
-import com.noyize.deepsky.R
 import com.noyize.deepsky.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import dev.chrisbanes.insetter.applyInsetter
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        WindowCompat.setDecorFitsSystemWindows(window,false)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-     //   setInsets()
-//        setSupportActionBar(binding.toolbar)
-//        setUpNavigation()
     }
-
-//    private fun setInsets(){
-//        binding.toolbar.applyInsetter {
-//            type(statusBars = true){
-//                padding()
-//            }
-//        }
-//    }
-//
-//    private fun setUpNavigation(){
-//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
-//        val navController = navHostFragment.navController
-//        binding.toolbar.setupWithNavController(navController)
-//    }
 }
